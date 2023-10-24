9638247a7a587cf6b66390c4f9d177765a1589b8
Mariana <marianaarevalo933@gmail.com>
2023-10-23 23:22:03 -0700

    secciones con elementos completas + integracion del formulario

--- js/main.js
+++ js/main.js
@@ -1,18 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
-  
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        scrollToBottomButtom.style.display = 'block';
-      } else {
-        scrollToBottomButtom.style.display = 'none';
-      }
-    })
-  
-    scrollToBottomButtom.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      })
-    })
-  })
+  const scrollToTopButton = document.getElementById('btn-ir-arriba');
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      scrollToTopButton.style.display = 'block';
+    } else {
+      scrollToTopButton.style.display = 'none';
+    }
+  });
+
+  scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  const carousel = document.querySelector('.carousel');
+  const slides = carousel.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateCarousel();
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    updateCarousel();
+  }
+
+  function updateCarousel() {
+    const offset = -currentSlide * 100 + '%';
+    carousel.style.transform = `translateX(${offset})`;
+  }
+
+  // Controladores de flechas
+  const nextButton = document.createElement('button');
+  nextButton.textContent = '>';
+  nextButton.addEventListener('click', nextSlide);
+  carousel.appendChild(nextButton);
+
+  const prevButton = document.createElement('button');
+  prevButton.textContent = '<';
+  prevButton.addEventListener('click', prevSlide);
+  carousel.appendChild(prevButton);
+
+  updateCarousel();
+});
